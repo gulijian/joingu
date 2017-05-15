7f5b8d6819dde186122baf86585c93745bc60c39
newchama <1175327069@qq.com>
2017-05-15 14:37:57 +0800

    抽离属性配置文件

---
+++ gulj-app-blog-biz/src/main/java/com/gulj/app/blog/biz/mapper/BlogArticleMapper.java
@@ -0,0 +1,7 @@
+package com.gulj.app.blog.biz.mapper;
+
+import com.gulj.app.blog.api.entity.BlogArticle;
+import com.gulj.app.blog.biz.JoinGuMapper;
+
+public interface BlogArticleMapper extends JoinGuMapper<BlogArticle> {
+}
